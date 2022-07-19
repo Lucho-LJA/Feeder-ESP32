59f515323546f63f8099132c7f104318871e0380
Lucho-lja <ljallauca.a@gmail.com>
2022-07-18 22:31:38 -0500

    Add config to FEEDER_2SENSOR_1MOTOR

--- include/ROS_CONFIG.h
+++ include/ROS_CONFIG.h
@@ -24,5 +24,38 @@
     const uint16_t serverPort = ROS_SERVER_PORT;
 
     ros::NodeHandle nh;
+    //name to devise
+    String name_robot = DEVISE_NAME;
+    #ifdef FEEDER_2SENSOR_1MOTOR
+        // VARIABLES TO TOPICS
+        String pSensor1=name_robot+"/get_sensor_piece";
+        String pSensor2=name_robot+"/get_sensor_turn";
+        String pState=name_robot+"/get_state";
+        String sState=name_robot+"/set_state";
+        String sVel=name_robot+"/set_vel";
+        
+
+        //FUNCTIONS TO SYBSCRIPTORS
+        void ReadSetPoint( const std_msgs::Int16& msg){
+            setMotor= msg.data;
+        }
+        void ReadState( const std_msgs::Int8& msg){
+            stateDevise= msg.data;
+        }
+        
+
+        //MAKE VARIABLES TO PUBLISHERS
+        std_msgs::Int8 sensor1_msg;
+        std_msgs::Int8 sensor2_msg;
+
+        //BUILD PUBLISHER
+        ros::Publisher pDeviseSensor1(pSensor1.c_str(), &sensor1_msg);
+        ros::Publisher pDeviseSensor2(pSensor2.c_str(), &sensor2_msg);
+
+        //BUILD SUBSCRIPTORS
+        ros::Subscriber<std_msgs::Int16> sDeviseSetMotor(sVel.c_str(),&ReadSetPoint);
+        ros::Subscriber<std_msgs::Int8> sDeviseState(sState.c_str(),&ReadState);
+
+    #endif
     
 #endif
